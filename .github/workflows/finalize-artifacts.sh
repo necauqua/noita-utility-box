d9916f31bd3f99409f5166ce6b781c9e82ca2911
Anton Bulakh <him@necauq.ua>
2025-03-06 01:08:14 +0200

    ci: increase test job timeout and fix linux-generic artifact

--- .github/workflows/finalize-artifacts.sh
+++ .github/workflows/finalize-artifacts.sh
@@ -5,7 +5,7 @@ set -e
 cp -r --no-preserve=mode,ownership result-2/ linux-generic
 chmod +x linux-generic/bin/noita-utility-box
 pushd linux-generic
-tar -czf ../noita-utility-box-linux-generic.tar *
+tar -czf ../noita-utility-box-linux-generic.tar.gz *
 popd
 
 cp --no-preserve=mode,ownership result-1/bin/noita-utility-box.exe .
