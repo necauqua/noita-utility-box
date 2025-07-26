e1568bdc316fcabfc1fd0d868fde7b5b7059bfc3
Anton Bulakh <him@necauq.ua>
2025-07-26 04:56:19 +0300

    reader: remove the detection evasion as it wasn't all that useful

--- noita-engine-reader/src/memory/process_ref.rs
+++ noita-engine-reader/src/memory/process_ref.rs
@@ -131,7 +131,7 @@ mod platform {
     use windows::Win32::System::{
         Diagnostics::Debug::ReadProcessMemory,
         ProcessStatus::EnumProcessModules,
-        Threading::{PROCESS_QUERY_INFORMATION, PROCESS_VM_READ},
+        Threading::{OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ},
     };
 
     mod threadsafe_handle {
@@ -147,12 +147,12 @@ mod platform {
         /// > is closed with CloseHandle().
         ///
         /// from this one guy on https://stackoverflow.com/a/12214212
-        ///
-        /// Handle is an opaque number, it's just that in Windows Rust API they
-        /// made it !Send and !Sync because it is indeed not always threadsafe
-        /// I think?.
         #[derive(Debug)]
         pub struct ThreadsafeHandle(Owned<HANDLE>);
+
+        // Handle is an opaque number, it's just that in Windows Rust API they
+        // made it !Send and !Sync because it is indeed not always threadsafe
+        // I think?.
         unsafe impl Send for ThreadsafeHandle {}
         unsafe impl Sync for ThreadsafeHandle {}
 
@@ -192,8 +192,10 @@ mod platform {
 
     impl Handle {
         pub fn connect(pid: u32) -> io::Result<Self> {
-            let handle = open_process(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, pid)
-                .map_err(better_message)?;
+            let handle =
+                unsafe { OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, pid) }
+                    .map(|h| unsafe { ThreadsafeHandle::new(h) })
+                    .map_err(better_message)?;
 
             let mut module = unsafe { std::mem::zeroed() };
             let mut cb_needed = 0;
@@ -237,125 +239,6 @@ mod platform {
             }
             .map_err(better_message)?;
             Ok(())
-        }
-    }
-
-    #[cfg(not(feature = "sneaky"))]
-    fn open_process(
-        access: windows::Win32::System::Threading::PROCESS_ACCESS_RIGHTS,
-        pid: u32,
-    ) -> windows::core::Result<ThreadsafeHandle> {
-        use windows::Win32::System::Threading::OpenProcess;
-
-        unsafe { OpenProcess(access, false, pid).map(|h| ThreadsafeHandle::new(h)) }
-    }
-
-    #[cfg(feature = "sneaky")]
-    use direct_syscall::open_process;
-
-    /// A quick and dirty copypaste of the "Hells Gate" technique
-    /// (<https://fluxsec.red/rust-edr-evasion-hells-gate>)
-    /// to hopefully stop Windows Defender from being annoying
-    /// by *LITERALLY* employing detection evasion rofl
-    ///
-    /// The idea is that we don't link to NtOpenProcess (which is sus) and
-    /// instead do a complicated pointer dance to figure out it's address
-    /// without linking to other sus things (thankfully that one is handled by
-    /// `export_resolver` crate from the author of the blog post)
-    /// and then from that we get the syscall number (which are not static) for
-    /// the NtOpenProcess syscall, which we proceed to manually invoke with
-    /// (more) inline assembly.
-    #[cfg(feature = "sneaky")]
-    mod direct_syscall {
-        use std::arch::asm;
-        use str_crypter::{decrypt_string, sc};
-        use windows::{
-            Wdk::Foundation::OBJECT_ATTRIBUTES,
-            Win32::{
-                Foundation::{HANDLE, NTSTATUS},
-                System::Threading::PROCESS_ACCESS_RIGHTS,
-                System::WindowsProgramming::CLIENT_ID,
-            },
-        };
-
-        use super::ThreadsafeHandle;
-
-        pub fn open_process(
-            access: PROCESS_ACCESS_RIGHTS,
-            pid: u32,
-        ) -> windows::core::Result<ThreadsafeHandle> {
-            let mut process_handle = HANDLE::default();
-
-            let mut object_attributes = OBJECT_ATTRIBUTES {
-                Length: size_of::<OBJECT_ATTRIBUTES>() as u32,
-                ..Default::default() // zeroed
-            };
-            let mut client_id = CLIENT_ID {
-                UniqueProcess: HANDLE(pid as _),
-                ..Default::default() // zeroed
-            };
-
-            let status = unsafe {
-                nt_open_process(
-                    &mut process_handle, // out
-                    access.0,
-                    &mut object_attributes,
-                    &mut client_id, // contains the pid
-                )
-            };
-            if status.is_ok() {
-                Ok(unsafe { ThreadsafeHandle::new(process_handle) })
-            } else {
-                Err(windows::core::Error::from_win32())
-            }
-        }
-
-        static SSN: std::sync::LazyLock<Option<u32>> = std::sync::LazyLock::new(|| {
-            let mut exports = export_resolver::ExportList::new();
-
-            // don't put sus strings in .rdata (also result returned can never be Err)
-            let op = sc!("NtOpenProcess", 42).unwrap();
-            let dll = sc!("ntdll.dll", 42).unwrap();
-
-            // the api is a bit weird for like caching purposes I suppose
-            exports.add(&dll, &op).ok()?;
-            // never fails if above succeeded
-            let f = exports.get_function_address(&op).unwrap();
-
-            let ssn: u16 = unsafe { *(f as *const u8).add(4).cast() };
-
-            if ssn != 38 {
-                tracing::warn!(ssn, "Weird SSN, on last Windows versions it should be 38");
-            }
-
-            Some(ssn as _)
-        });
-
-        unsafe fn nt_open_process(
-            process_handle: *mut HANDLE,
-            desired_access: u32,
-            object_attributes: *mut OBJECT_ATTRIBUTES,
-            client_id: *mut CLIENT_ID,
-        ) -> NTSTATUS {
-            let ssn = SSN.expect("SSN not found");
-
-            let status: i32;
-            unsafe {
-                asm!(
-                    "mov r10, rcx",
-                    "mov eax, {0:e}", // move the syscall number into EAX
-                    "syscall",
-                    in(reg) ssn, // input: Syscall number goes into EAX
-                    // Order: https://web.archive.org/web/20170222171451/https://msdn.microsoft.com/en-us/library/9z1stfyw.aspx
-                    in("rcx") process_handle,   // passed to RCX (first argument)
-                    in("rdx") desired_access,   // passed to RDX (second argument)
-                    in("r8") object_attributes, // passed to R8 (third argument)
-                    in("r9") client_id,         // passed to R9 (fourth argument)
-                    lateout("rax") status,      // output: returned value of the syscall is placed in RAX
-                    options(nostack),           // dont modify the stack pointer
-                );
-            }
-            NTSTATUS(status)
         }
     }
 }
